4ac38505e82d408d5b52672729a940d73efa2e21
Nico Vonau <vonaun@student.ethz.ch>
2020-11-21 10:09:35 +0100

    deploy

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,8 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/register', {
+		// fetch('http://localhost:3000/register', {
+		fetch('https://floating-hollows-13415.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
